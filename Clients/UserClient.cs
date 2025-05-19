c4965530be2d15fa4506ee76035829686f16d3cf
Georgi Dimitrov <georgi.dimitrov1170@gmail.com>
2025-05-19 13:53:29 +0300

    Add API test and ignore .vs folder

---
+++ Clients/UserClient.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace api_test.Clients
+{
+    internal class UserClient
+    {
+    }
+}
---
+++ Tests/UserTests.cs
@@ -0,0 +1,38 @@
+﻿using NUnit.Framework;
+using RestSharp;
+using Newtonsoft.Json.Linq;
+
+namespace Tests
+{
+    public class UserTests
+    {
+        private RestClient client;
+
+        [SetUp]
+        public void Setup()
+        {
+            client = new RestClient("https://reqres.in/");
+        }
+
+        [Test]
+        public void GetUsers_ShouldReturnListOfUsers()
+        {
+            var request = new RestRequest("api/users?page=2", Method.Get);
+            var response = client.Execute(request);
+
+            Assert.That(response.IsSuccessful, Is.True, "Request failed");
+            Assert.That(response.StatusCode, Is.EqualTo(System.Net.HttpStatusCode.OK));
+
+            var json = JObject.Parse(response.Content);
+            var data = json["data"];
+
+            Assert.That(data.HasValues, Is.True, "No users returned");
+        }
+        [TearDown]
+        public void TearDown() 
+        {
+           client.Dispose();
+        }
+
+    }
+}
